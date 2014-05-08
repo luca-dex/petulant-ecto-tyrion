2112d48b1ff94fcfa6269b7bb0b90ea34bee5339
ALTree <alb.donizetti@gmail.com>
2014-05-08 13:50:22 +0200

    Use the buildin sciy reader for sparse matrices

--- petulant.py
+++ petulant.py
@@ -12,35 +12,61 @@
 # All rights reserved.
 # See LICENSE.txt
 
-from scipy.sparse import *
-from scipy import *
+import os
+from scipy.io import mminfo, mmread 
 
-def read_matrix():
-	f = open("simmetrica-46902.dat", "r")
-	n, non_blank, form = f.readline().split()
-	n = int(n)
-	non_blank = int(non_blank)
-	S = dok_matrix((n, n), dtype=float32)
+## change .dat file to Matrix Market Format
+def change_format(filename):
+    f = open(filename)
+    
+    comment = "%%MatrixMarket matrix coordinate real "
+    if "non" in f.name:
+        comment += "unsymmetric\n"
+    else:
+        comment += "symmetric\n"
+        
+    n, l, form = f.readline().split()
+    form = "" + n + " " + n + " " + l + "\n"
+        
+    f_new = open("mtx_files/" + filename.replace("dat", "mtx"), "w")
+        
+    f_new.write(comment)
+    f_new.write(form)
+    for line in f.readlines():
+        f_new.write(line)
+                
+    f.close()
+    f_new.close()
 
-	for line in f:
-	    x, y, v = line.split()
-	    x = int(x) - 1
-	    y = int(y) - 1
-	    v = float(v)
-	    try:
-	    	S[x, y] = v
-	    except:
-	    	print('x:', x, 'y: ', y, 'v: ', v)
 
-	return S
-	    	
-
+## change to Matrix Market Format all files in the current dir
+def change_all():
+    if not os.path.exists("mtx_files"):
+        os.mkdir("mtx_files")
+        
+    for filename in os.listdir('.'):
+        if "dat" in filename:
+            change_format(filename)
+                
+## read sparse matrix in .mtx file
+def read_matrix_scipy(filename):
+    return mmread(filename)
 
 
 def main():
-	S = read_matrix()
-	return S
+    # run me if you don't have the .mtx files
+    # change_all() 
+
+    os.chdir("mtx_files")
+        
+    filename = "non-simmetrica-46902.mtx"
+
+    print "Matrix is shaped like this:"
+    print mminfo(filename)
+
+    print "\nNow let's read the matrix:"
+    print read_matrix_scipy(filename).data
 
 
 if __name__ == '__main__':
-	S = main()
+    main()
