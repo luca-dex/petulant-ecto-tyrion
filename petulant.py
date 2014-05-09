ee22d7c69207312d075247e909543fb0e9c71182
luca-dex <luca.desano@gmail.com>
2014-05-09 02:16:28 +0200

    così è più bello

--- petulant.py
+++ petulant.py
@@ -15,32 +15,35 @@
 import os
 from scipy.io import mminfo, mmread 
 
-## change .dat file to Matrix Market Format
 def change_format(filename):
-    f = open(filename)
+    """
+    change .dat file to Matrix Market Format
+    """
+    with open(filename) as f:
     
-    comment = "%%MatrixMarket matrix coordinate real "
-    if "non" in f.name:
-        comment += "unsymmetric\n"
-    else:
-        comment += "symmetric\n"
-        
-    n, l, form = f.readline().split()
-    form = "" + n + " " + n + " " + l + "\n"
-        
-    f_new = open("mtx_files/" + filename.replace("dat", "mtx"), "w")
-        
-    f_new.write(comment)
-    f_new.write(form)
-    for line in f.readlines():
-        f_new.write(line)
-                
-    f.close()
-    f_new.close()
+        comment = "%%MatrixMarket matrix coordinate real "
+        if "non" in f.name:
+            comment += "unsymmetric\n"
+        else:
+            comment += "symmetric\n"
+            
+        n, l, form = f.readline().split()
+        form = "" + n + " " + n + " " + l + "\n"
+          
+        filename_new = "mtx_files/" + filename.replace("dat", "mtx")
+
+        with open(filename_new, "w") as f_new:
+            
+            f_new.write(comment)
+            f_new.write(form)
+            for line in f:
+                f_new.write(line)
 
 
-## change to Matrix Market Format all files in the current dir
 def change_all():
+    """
+    change to Matrix Market Format all files in the current dir
+    """
     if not os.path.exists("mtx_files"):
         os.mkdir("mtx_files")
         
@@ -55,17 +58,17 @@ def read_matrix_scipy(filename):
 
 def main():
     # run me if you don't have the .mtx files
-    # change_all() 
+    change_all() 
 
     os.chdir("mtx_files")
         
-    filename = "non-simmetrica-46902.mtx"
+    filename = "simmetrica-46902.mtx"
 
-    print "Matrix is shaped like this:"
-    print mminfo(filename)
+    print("Matrix is shaped like this:")
+    print(mminfo(filename))
 
-    print "\nNow let's read the matrix:"
-    print read_matrix_scipy(filename).data
+    print("\nNow let's read the matrix:")
+    print(read_matrix_scipy(filename).data)
 
 
 if __name__ == '__main__':
